59a889b6a4947234aa1e876cdd4c6adcc5491d3e
Arek Wtykło <arek.wtyklo@arteneoit.pl>
2018-04-12 11:00:55 +0200

    HTML insert

--- web/template/js/main.js
+++ web/template/js/main.js
@@ -1,31 +1,41 @@
 // Przykładowa tablica
 var arr = ["Alfa Romeo", "Skoda", "Ferrari", "Fiat", "Volkswagen", "Kamaz", "Audi", "Mercedes"];
 
+updateView();
 
-drawArrayInfo(arr);
-
-drawBox(arr[0]);
-drawBox(arr[1]);
-drawBox(arr[2]);
-drawBox(arr[3]);
-drawBox(arr[4]);
-drawBox(arr[5]);
-drawBox(arr[6]);
-drawBox(arr[7]);
-drawBox(arr[7]);
-
+function updateView() {
+    var arrayElementsDOM = document.getElementById('array-elements');
+    // Usunie mi istniejace boxy
+    arrayElementsDOM.innerHTML = "";
+    // Narysuje mi je ponownie
+    arr.forEach(drawBox);
+    // Aktualizuje informacje o tablicy
+    drawArrayInfo(arr);
+}
 
 // Pokazujemy informacje o tablicy
 function drawArrayInfo(arr) {
     console.log('Rysuję informację o tablicy:');
     console.log(arr);
     var arrayElementsDOM = document.getElementById('array');
+    arrayElementsDOM.innerHTML = "";
     arrayElementsDOM.insertAdjacentHTML('beforeend', '<h3><small class="text-muted pr-2">Length:</small>' + arr.length + '</h3>');
 }
 
 // Rysowanie boxu z informacja
-function drawBox(arrValue) {
-    console.log('Rysuję BOX dla wartości "' + arrValue + '"');
+function drawBox(arrValue, index) {
+    console.log('Rysuję BOX dla wartości "' + arrValue + '" [klucz = ' + index + ']');
     var arrayElementsDOM = document.getElementById('array-elements');
-    arrayElementsDOM.insertAdjacentHTML('beforeend', '<div class="col-3"><div class="box"><h2>' + arrValue + '</h2></div></div>');
+    var html = '<div class="col-3">\n\
+<div class="box">\n\
+<div class="box-actions">\n\
+<button class="btn btn-danger btn-sm" onclick="removeElement(\'' + arrValue + '\')">\n\
+<i class="far fa-trash-alt"></i>\n\
+</button>\n\
+</div>\n\
+<h2>' + arrValue + '</h2>\n\
+</div>\n\
+</div>';
+    console.log(html);
+    arrayElementsDOM.insertAdjacentHTML('beforeend', html);
 }
